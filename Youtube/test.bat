c2ebebcfcb27a6fde40398e21588ad7ead1be1a2
officenne <officenne@gmail.com>
2020-09-13 11:20:55 +0000

    file updated

--- Youtube/test.bat
+++ Youtube/test.bat
@@ -1,12 +1,13 @@
 cd C:\Users\medisaapps\Public\Youtube
 
-curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCk8GzjMOrta8yxDcKfylJYw&type=video&order=date&maxResults=100&key=AIzaSyAg7MFGsRE-FpSoxqgz1eXGX5OSO2oeKH8&pageToken=CDIQAA" > file2.json
+curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCk8GzjMOrta8yxDcKfylJYw&type=video&order=date&maxResults=100&key=AIzaSyAg7MFGsRE-FpSoxqgz1eXGX5OSO2oeKH8&pageToken=CDIQAA" > file7.json
 
 git add -A
 
 
 git commit -m "file updated "
 
+git pull
 
 git push origin master
 
