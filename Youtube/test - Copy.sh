a589d9820046b4fcbb885c20142151528fe43ce8
officenne <officenne@gmail.com>
2020-09-13 11:14:17 +0000

    file updated

--- Youtube/test - Copy.sh
+++ Youtube/test - Copy.sh
@@ -1,4 +1,4 @@
-cd C:\Users\medisaapps\Public\Youtube
+#cd C:\Users\medisaapps\Public\Youtube
 
 curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCk8GzjMOrta8yxDcKfylJYw&type=video&order=date&maxResults=100&key=AIzaSyAg7MFGsRE-FpSoxqgz1eXGX5OSO2oeKH8&pageToken=CDIQAA" > file3.json
 
