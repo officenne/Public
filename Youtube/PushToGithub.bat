355740e3923f184b972f8ea6038e00854b29de40
officenne <officenne@gmail.com>
2020-09-14 05:36:40 +0000

    file updated

--- Youtube/PushToGithub.bat
+++ Youtube/PushToGithub.bat
@@ -1,6 +1,6 @@
 cd C:\Users\medisaapps\Public\Youtube
 
-curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCk8GzjMOrta8yxDcKfylJYw&type=video&order=date&maxResults=100&key=AIzaSyD0R-Aw-3fqTpxVV6BklpTzXd9yICBZszQ&pageToken=CDIQAA" > file8.json
+curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCk8GzjMOrta8yxDcKfylJYw&type=video&order=date&maxResults=100&key=AIzaSyCYxmlE2_KKAPQkbo2TnyrzS_ujoVwBsrE&pageToken=CDIQAA" > file8.json
 
 git add -A
 
