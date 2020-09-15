15aa0e9454382a0b5170ec5e8db133c0598e8f61
officenne <officenne@gmail.com>
2020-09-15 11:36:19 +0000

    file updated

--- Youtube/PushToGithub.bat
+++ Youtube/PushToGithub.bat
@@ -2,6 +2,14 @@ cd C:\Users\medisaapps\Public\Youtube
 
 curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCPOw2O3_uZ1doro9iR4x6vw&type=video&order=date&maxResults=100&key=AIzaSyCYxmlE2_KKAPQkbo2TnyrzS_ujoVwBsrE&pageToken=CDIQAA" > mmoshaya.json
 
+curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCmIp3opkQ6cEeJ6ybGrC_2A&type=video&order=date&maxResults=100&key=AIzaSyCYxmlE2_KKAPQkbo2TnyrzS_ujoVwBsrE&pageToken=CDIQAA" > mmoshaya2.json
+
+
+
+
+
+
+
 git add -A
 
 
