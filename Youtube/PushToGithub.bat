59b57d155458dfd4f04169b4a86626c466cf4e2c
officenne <officenne@gmail.com>
2020-09-16 09:39:14 +0000

    file updated

--- Youtube/PushToGithub.bat
+++ Youtube/PushToGithub.bat
@@ -8,7 +8,9 @@ curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCmIp3
 curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCR60erWn2_8aoaLE3OoVu4w&type=video&order=date&maxResults=100&key=AIzaSyCYxmlE2_KKAPQkbo2TnyrzS_ujoVwBsrE" > Iman_Moshaya.json
 
 
+cd C:\Users\medisaapps\Public\Youtube\KidsDianaShow
 
+curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCk8GzjMOrta8yxDcKfylJYw&type=video&order=date&maxResults=100&key=AIzaSyCYxmlE2_KKAPQkbo2TnyrzS_ujoVwBsrE" > KidsDianaShow.json
 
 
 git add -A
