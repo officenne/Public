65d7698ee3efa15a433321933a44768ac1878b81
officenne <officenne@gmail.com>
2020-11-16 19:35:09 +0000

    file updated

--- PushToGithub.bat
+++ PushToGithub.bat
@@ -27,6 +27,7 @@ curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCGgeD
 
 
 cd C:\Users\medisaapps\Public\Android\EgyptApps\KidsShows
+
 curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCk8GzjMOrta8yxDcKfylJYw&type=video&order=date&maxResults=100&key=AIzaSyCYxmlE2_KKAPQkbo2TnyrzS_ujoVwBsrE" > KidsDianaShow.json
 curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCbCmjCuTUZos6Inko4u57UQ&type=video&order=date&maxResults=100&key=AIzaSyCYxmlE2_KKAPQkbo2TnyrzS_ujoVwBsrE" > Cocomelon.json
 curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCJplp5SjeGSdVdwsfb9Q7lQ&type=video&order=date&maxResults=100&key=AIzaSyCYxmlE2_KKAPQkbo2TnyrzS_ujoVwBsrE" >  LikeNastya.json
@@ -35,6 +36,19 @@ curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCvlE5
 curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCBnZ16ahKA2DZ_T5W0FPUXg&type=video&order=date&maxResults=100&key=AIzaSyCYxmlE2_KKAPQkbo2TnyrzS_ujoVwBsrE" > ChuChuTV.json
 curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCgFXm4TI8htWmCyJ6cVPG_A&type=video&order=date&maxResults=100&key=AIzaSyCYxmlE2_KKAPQkbo2TnyrzS_ujoVwBsrE" > ToysandColors.json
 curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCfaZw8XH_zmAVkBst_MPD6w&type=video&order=date&maxResults=100&key=AIzaSyCYxmlE2_KKAPQkbo2TnyrzS_ujoVwBsrE" > CKNToys.json
+
+
+
+cd C:\Users\medisaapps\Public\Android\EgyptApps\Families
+
+
+
+curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCYogOW28QIQdfvfY5KyzDsw&type=video&order=date&maxResults=100&key=AIzaSyCYxmlE2_KKAPQkbo2TnyrzS_ujoVwBsrE" > Badr_Family.json
+curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCsc0cXTWBI7suJmQXvfs5Hw&type=video&order=date&maxResults=100&key=AIzaSyCYxmlE2_KKAPQkbo2TnyrzS_ujoVwBsrE" > riyadrzq.json
+curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC_Bd5Yrn9kL8fisLfPmH5ww&type=video&order=date&maxResults=100&key=AIzaSyCYxmlE2_KKAPQkbo2TnyrzS_ujoVwBsrE" >  feehan.json
+
+
+
 
 
 git add -A
