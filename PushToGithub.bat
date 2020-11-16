75ff1a58dee6089048050c7354a9d5dab203b471
officenne <officenne@gmail.com>
2020-11-16 20:13:01 +0000

    file updated

--- PushToGithub.bat
+++ PushToGithub.bat
@@ -40,12 +40,11 @@ curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCsc0c
 curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC_Bd5Yrn9kL8fisLfPmH5ww&type=video&order=date&maxResults=100&key=AIzaSyDVXHy2D-umGU-ExrjJr62tmovs5BBXh44" >  feehan.json
 
 curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCPOw2O3_uZ1doro9iR4x6vw&type=video&order=date&maxResults=100&key=AIzaSyDVXHy2D-umGU-ExrjJr62tmovs5BBXh44" > mmoshaya.json
-
 curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCmIp3opkQ6cEeJ6ybGrC_2A&type=video&order=date&maxResults=100&key=AIzaSyDVXHy2D-umGU-ExrjJr62tmovs5BBXh44" > mmoshaya2.json
-
 curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCR60erWn2_8aoaLE3OoVu4w&type=video&order=date&maxResults=100&key=AIzaSyDVXHy2D-umGU-ExrjJr62tmovs5BBXh44" > Iman_Moshaya.json
 
-
+curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCfZVRPD9O6rlSFnanpNXG4A&type=video&order=date&maxResults=100&key=AIzaSyDVXHy2D-umGU-ExrjJr62tmovs5BBXh44" > Yazan_Family.json
+curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCqJNTGwKmgMCwfYDbnz514Q&type=video&order=date&maxResults=100&key=AIzaSyDVXHy2D-umGU-ExrjJr62tmovs5BBXh44" > anasala_family.json
 
 
 git add -A
