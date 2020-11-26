fcd736d3b1e7d8a704eb1e5df48cc755512981ab
officenne <officenne@gmail.com>
2020-11-26 10:15:54 +0000

    file updated

--- PushToGithub.bat
+++ PushToGithub.bat
@@ -47,6 +47,25 @@ curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCfZVR
 curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCqJNTGwKmgMCwfYDbnz514Q&type=video&order=date&maxResults=100&key=AIzaSyDVXHy2D-umGU-ExrjJr62tmovs5BBXh44" > anasala_family.json
 
 
+
+
+
+
+
+
+cd C:\Users\medisaapps\Public\Android\EgyptApps\Madrasati
+
+curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCndb1LGM5oQJVhjh2NViU5g&type=video&order=date&maxResults=100&key=AIzaSyCghMwZZstjt92Zx5Mep8V2_aCvvVdkI1A" > DorosIEN.json
+
+curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCL-2Lb4n3uiKbmLu9q9KroQ&type=video&order=date&maxResults=100&key=AIzaSyCghMwZZstjt92Zx5Mep8V2_aCvvVdkI1A" > Madrasati.json
+
+
+
+
+
+
+
+
 git add -A
 
 
