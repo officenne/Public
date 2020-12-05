5a1b6f1c7bc6ea8b79dc595205843a19528d9f3e
officenne <officenne@gmail.com>
2020-12-05 12:51:43 +0000

    file updated

---
+++ PushToGithub - Copy.bat
@@ -0,0 +1,18 @@
+git pull
+
+
+
+
+
+
+git add -A
+
+
+git commit -m "file updated "
+
+
+
+git push origin master
+
+
+
--- PushToGithub.bat
+++ PushToGithub.bat
@@ -8,34 +8,6 @@ curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCk8Gz
 
 
 
-cd C:\Users\medisaapps\Public\Android\EgyptApps\Madrestna
-
-curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCnxju7_Ug6VbC6en6tBL6Aw&type=video&order=date&maxResults=100&key=AIzaSyCT6mFtx_a5gqWpvjnSnUS7lDd6wrrLmgI" > Madrestna_Channel.json
-
-
-curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCnxju7_Ug6VbC6en6tBL6Aw&type=video&eventType=live&key=AIzaSyCT6mFtx_a5gqWpvjnSnUS7lDd6wrrLmgI" > Madrestna_Channel_Live.json
-
-
-
-curl "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&channelId=UCnxju7_Ug6VbC6en6tBL6Aw&q=%D8%A7%D9%84%D8%B5%D9%81+%D8%A7%D9%84%D8%B1%D8%A7%D8%A8%D8%B9+%D8%A7%D9%84%D8%A7%D8%A8%D8%AA%D8%AF%D8%A7%D8%A6%D9%89&maxResults=50&order=date&key=AIzaSyCT6mFtx_a5gqWpvjnSnUS7lDd6wrrLmgI" > Madrestna_P4.json
-
-
-curl "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&channelId=UCnxju7_Ug6VbC6en6tBL6Aw&q=%D8%A7%D9%84%D8%B5%D9%81+%D8%A7%D9%84%D8%AE%D8%A7%D9%85%D8%B3+%D8%A7%D9%84%D8%A7%D8%A8%D8%AA%D8%AF%D8%A7%D8%A6%D9%89&maxResults=50&order=date&key=AIzaSyCT6mFtx_a5gqWpvjnSnUS7lDd6wrrLmgI" > Madrestna_P5.json
-
-curl "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&channelId=UCnxju7_Ug6VbC6en6tBL6Aw&q=%D8%A7%D9%84%D8%B5%D9%81+%D8%A7%D9%84%D8%B3%D8%A7%D8%AF%D8%B3+%D8%A7%D9%84%D8%A7%D8%A8%D8%AA%D8%AF%D8%A7%D8%A6%D9%89&maxResults=50&order=date&key=AIzaSyCT6mFtx_a5gqWpvjnSnUS7lDd6wrrLmgI" > Madrestna_P6.json
-
-curl "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&channelId=UCnxju7_Ug6VbC6en6tBL6Aw&q=%D8%A7%D9%84%D8%B5%D9%81+%D8%A7%D9%84%D8%A7%D9%88%D9%84+%D8%A7%D9%84%D8%A7%D8%B9%D8%AF%D8%A7%D8%AF%D9%89&maxResults=50&order=date&key=AIzaSyCT6mFtx_a5gqWpvjnSnUS7lDd6wrrLmgI" > Madrestna_M1.json
-
-curl "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&channelId=UCnxju7_Ug6VbC6en6tBL6Aw&q=%D8%A7%D9%84%D8%B5%D9%81+%D8%A7%D9%84%D8%AB%D8%A7%D9%86%D9%8A+%D8%A7%D9%84%D8%A5%D8%B9%D8%AF%D8%A7%D8%AF%D9%89&maxResults=50&order=date&key=AIzaSyCT6mFtx_a5gqWpvjnSnUS7lDd6wrrLmgI" > Madrestna_M2.json
-
-curl "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&channelId=UCnxju7_Ug6VbC6en6tBL6Aw&q=%D8%A7%D9%84%D8%B5%D9%81+%D8%A7%D9%84%D8%AB%D8%A7%D9%84%D8%AB+%D8%A7%D9%84%D8%A7%D8%B9%D8%AF%D8%A7%D8%AF%D9%89&maxResults=50&order=date&key=AIzaSyCT6mFtx_a5gqWpvjnSnUS7lDd6wrrLmgI" > Madrestna_M3.json
-
-
-
-
-
-
-
 
 
 cd C:\Users\medisaapps\Public\Android\EgyptApps\KidsShows
