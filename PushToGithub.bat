59c16e50cf3d3a1a60447dccfbee578ac7139d82
officenne <officenne@gmail.com>
2020-12-05 10:16:52 +0000

    file updated

--- PushToGithub.bat
+++ PushToGithub.bat
@@ -10,10 +10,10 @@ curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCk8Gz
 
 cd C:\Users\medisaapps\Public\Android\EgyptApps\Madrestna
 
-curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCGgeDgjIJ3GXZcLUnExAybw&type=video&order=date&maxResults=100&key=AIzaSyCYxmlE2_KKAPQkbo2TnyrzS_ujoVwBsrE" > Madrestna_Channel.json
+curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCGgeDgjIJ3GXZcLUnExAybw&type=video&order=date&maxResults=100&key=AIzaSyCT6mFtx_a5gqWpvjnSnUS7lDd6wrrLmgI" > Madrestna_Channel.json
 
 
-curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCGgeDgjIJ3GXZcLUnExAybw&type=video&eventType=live&key=AIzaSyCYxmlE2_KKAPQkbo2TnyrzS_ujoVwBsrE" > Madrestna_Channel_Live.json
+curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCGgeDgjIJ3GXZcLUnExAybw&type=video&eventType=live&key=AIzaSyCT6mFtx_a5gqWpvjnSnUS7lDd6wrrLmgI" > Madrestna_Channel_Live.json
 
 
 
