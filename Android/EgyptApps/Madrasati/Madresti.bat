6c5a0392e30f37a0fd38bf4cd9e01941d096460b
Officenne <officenne@gmail.com>
2020-12-17 12:44:44 +0000

    file updated

---
+++ Android/EgyptApps/Madrasati/Madresti.bat
@@ -0,0 +1,25 @@
+git pull
+
+
+curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCndb1LGM5oQJVhjh2NViU5g&type=video&order=date&maxResults=100&key=AIzaSyCghMwZZstjt92Zx5Mep8V2_aCvvVdkI1A" > DorosIEN.json
+
+curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCL-2Lb4n3uiKbmLu9q9KroQ&type=video&order=date&maxResults=100&key=AIzaSyCghMwZZstjt92Zx5Mep8V2_aCvvVdkI1A" > Madrasati.json
+
+
+
+
+
+
+
+
+git add -A
+
+
+git commit -m "file updated "
+
+
+
+git push origin master
+
+
+pause
