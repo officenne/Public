f341d9c088203291a481a697cc492ecf5f4de615
Officenne <officenne@gmail.com>
2021-01-05 13:15:44 +0000

    file updated

---
+++ Android/EgyptApps/anasalafamily/anasalafamily.py
@@ -0,0 +1,51 @@
+﻿import urllib.request as request
+import json
+import os.path
+
+# Get Latest from Youtube
+
+YoutubeApiKey = 'AIzaSyACJCHu-oaZUtC6gaygvqii3LiBvMrGWBY'
+YoutubeChannelID = 'UCqJNTGwKmgMCwfYDbnz514Q'
+YoutubeUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId='
+YoutubeUrl2 = '&type=video&order=date&maxResults=100&key='
+
+save_path = 'C:\GitHub\Android\EgyptApps\anasalafamily'
+
+
+with request.urlopen(
+        "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCqJNTGwKmgMCwfYDbnz514Q&type=video&order=date&maxResults=100&key=AIzaSyACJCHu-oaZUtC6gaygvqii3LiBvMrGWBY") as response:
+    if response.getcode() == 200:
+        source = response.read()
+        data = json.loads(source)
+        # print(data)
+        completeName = os.path.join(save_path, "anasala_Channel.json")
+        with open(completeName, 'w') as outfile:
+            json.dump(data, outfile, indent=4)
+    else:
+        print('An error occurred while attempting to retrieve data from the API.')
+
+
+with request.urlopen(
+        "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=100&playlistId=PL8HgrY8hTlCBOAlYdgGwgJFnF43YVXrbd&key=AIzaSyACJCHu-oaZUtC6gaygvqii3LiBvMrGWBY") as response:
+    if response.getcode() == 200:
+        source = response.read()
+        data = json.loads(source)
+        # print(data)
+        completeName = os.path.join(save_path, "Challenges.json")
+        with open(completeName, 'w') as outfile:
+            json.dump(data, outfile, indent=4)
+    else:
+        print('An error occurred while attempting to retrieve data from the API.')
+
+with request.urlopen(
+        "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=100&playlistId=PL8HgrY8hTlCBZB_hO_5epF2gKD9roSDkR&key=AIzaSyACJCHu-oaZUtC6gaygvqii3LiBvMrGWBY") as response:
+    if response.getcode() == 200:
+        source = response.read()
+        data = json.loads(source)
+        # print(data)
+        completeName = os.path.join(save_path, "Favorites.json")
+        with open(completeName, 'w') as outfile:
+            json.dump(data, outfile, indent=4)
+    else:
+        print('An error occurred while attempting to retrieve data from the API.')
+
