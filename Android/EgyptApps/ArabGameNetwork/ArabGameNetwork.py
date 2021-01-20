0b01ad985ee4e76825e9b2335d49cff87b50d5f4
Officenne <officenne@gmail.com>
2021-01-20 12:20:53 +0000

    file updated

--- Android/EgyptApps/ArabGameNetwork/ArabGameNetwork.py
+++ Android/EgyptApps/ArabGameNetwork/ArabGameNetwork.py
@@ -24,3 +24,17 @@ with request.urlopen(
     else:
         print('An error occurred while attempting to retrieve data from the API.')
 
+
+with request.urlopen(
+        "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCBAy1yyCcsjmNtQZTUpeXjA&type=video&order=date&maxResults=100&key=AIzaSyDhUCsJgoE0yIMc1Go_Alcyc15kDc_T-qI") as response:
+    if response.getcode() == 200:
+        source = response.read()
+        data = json.loads(source)
+        # print(data)
+        completeName = os.path.join(save_path, "SAYED.json")
+        with open(completeName, 'w') as outfile:
+            json.dump(data, outfile, indent=4)
+    else:
+        print('An error occurred while attempting to retrieve data from the API.')
+
+
