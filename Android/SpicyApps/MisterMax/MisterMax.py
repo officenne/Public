0fd06fd120bd00a7aac11392ee4ebde8562052eb
Officenne <officenne@gmail.com>
2021-01-31 13:04:58 +0000

    file updated

--- Android/SpicyApps/MisterMax/MisterMax.py
+++ Android/SpicyApps/MisterMax/MisterMax.py
@@ -25,3 +25,45 @@ with request.urlopen(
         print('An error occurred while attempting to retrieve data from the API.')
 
 
+
+with request.urlopen(
+        "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCcartHVtvAUzfajflyeT_Gg&type=video&order=date&maxResults=100&key=AIzaSyDnoxRyPtrO9We-Aa3ACMpluNag24-DLIc") as response:
+    if response.getcode() == 200:
+        source = response.read()
+        data = json.loads(source)
+        # print(data)
+        completeName = os.path.join(save_path, 'MissKaty.json')
+        with open(completeName, 'w') as outfile:
+            json.dump(data, outfile, indent=4)
+    else:
+        print('An error occurred while attempting to retrieve data from the API.')
+
+
+
+with request.urlopen(
+        "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCjB2ziXgFTei3xGr3avOIig&type=video&order=date&maxResults=100&key=AIzaSyDnoxRyPtrO9We-Aa3ACMpluNag24-DLIc") as response:
+    if response.getcode() == 200:
+        source = response.read()
+        data = json.loads(source)
+        # print(data)
+        completeName = os.path.join(save_path, 'MisterMaxPlay.json')
+        with open(completeName, 'w') as outfile:
+            json.dump(data, outfile, indent=4)
+    else:
+        print('An error occurred while attempting to retrieve data from the API.')
+
+
+
+with request.urlopen(
+        "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCvd_X-p3MEFQKoDGQjb3OiQ&type=video&order=date&maxResults=100&key=AIzaSyDnoxRyPtrO9We-Aa3ACMpluNag24-DLIc") as response:
+    if response.getcode() == 200:
+        source = response.read()
+        data = json.loads(source)
+        # print(data)
+        completeName = os.path.join(save_path, 'FedorUKVlogs.json')
+        with open(completeName, 'w') as outfile:
+            json.dump(data, outfile, indent=4)
+    else:
+        print('An error occurred while attempting to retrieve data from the API.')
+
+
