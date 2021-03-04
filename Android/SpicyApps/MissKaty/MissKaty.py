99f374a4709e9be1254c6451b698eb39c3ec6eee
Officenne <officenne@gmail.com>
2021-03-04 13:38:25 +0000

    file updated

--- Android/SpicyApps/MissKaty/MissKaty.py
+++ Android/SpicyApps/MissKaty/MissKaty.py
@@ -9,11 +9,11 @@ YoutubeChannelID = 'UCqJNTGwKmgMCwfYDbnz514Q'
 YoutubeUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId='
 YoutubeUrl2 = '&type=video&order=date&maxResults=100&key='
 
-save_path = 'C:\GitHub\Android\SpicyApps\MisterMax'
+save_path = 'C:\GitHub\Android\SpicyApps\MissKaty'
 
 
 with request.urlopen(
-        "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC_8PAD0Qmi6_gpe77S1Atgg&type=video&order=date&maxResults=100&key=AIzaSyDnoxRyPtrO9We-Aa3ACMpluNag24-DLIc") as response:
+        "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC_8PAD0Qmi6_gpe77S1Atgg&type=video&order=date&maxResults=100&key=AIzaSyCYhHtLs42EUKEq5Jc6fR80_oF2penoDNg") as response:
     if response.getcode() == 200:
         source = response.read()
         data = json.loads(source)
@@ -27,7 +27,7 @@ with request.urlopen(
 
 
 with request.urlopen(
-        "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCcartHVtvAUzfajflyeT_Gg&type=video&order=date&maxResults=100&key=AIzaSyDnoxRyPtrO9We-Aa3ACMpluNag24-DLIc") as response:
+        "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCcartHVtvAUzfajflyeT_Gg&type=video&order=date&maxResults=100&key=AIzaSyCYhHtLs42EUKEq5Jc6fR80_oF2penoDNg") as response:
     if response.getcode() == 200:
         source = response.read()
         data = json.loads(source)
@@ -41,7 +41,7 @@ with request.urlopen(
 
 
 with request.urlopen(
-        "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCjB2ziXgFTei3xGr3avOIig&type=video&order=date&maxResults=100&key=AIzaSyDnoxRyPtrO9We-Aa3ACMpluNag24-DLIc") as response:
+        "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCjB2ziXgFTei3xGr3avOIig&type=video&order=date&maxResults=100&key=AIzaSyCYhHtLs42EUKEq5Jc6fR80_oF2penoDNg") as response:
     if response.getcode() == 200:
         source = response.read()
         data = json.loads(source)
@@ -55,7 +55,7 @@ with request.urlopen(
 
 
 with request.urlopen(
-        "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCvd_X-p3MEFQKoDGQjb3OiQ&type=video&order=date&maxResults=100&key=AIzaSyDnoxRyPtrO9We-Aa3ACMpluNag24-DLIc") as response:
+        "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCvd_X-p3MEFQKoDGQjb3OiQ&type=video&order=date&maxResults=100&key=AIzaSyCYhHtLs42EUKEq5Jc6fR80_oF2penoDNg") as response:
     if response.getcode() == 200:
         source = response.read()
         data = json.loads(source)
@@ -69,7 +69,7 @@ with request.urlopen(
 
 
 with request.urlopen(
-        "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCufUZVlt_xWA2w0l_I2H6uw&type=video&order=date&maxResults=100&key=AIzaSyDnoxRyPtrO9We-Aa3ACMpluNag24-DLIc") as response:
+        "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCufUZVlt_xWA2w0l_I2H6uw&type=video&order=date&maxResults=100&key=AIzaSyCYhHtLs42EUKEq5Jc6fR80_oF2penoDNg") as response:
     if response.getcode() == 200:
         source = response.read()
         data = json.loads(source)
@@ -85,7 +85,7 @@ with request.urlopen(
 
 
 with request.urlopen(
-        "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCYKtRnIfwiJkVic89Yt51Pw&type=video&order=date&maxResults=100&key=AIzaSyDnoxRyPtrO9We-Aa3ACMpluNag24-DLIc") as response:
+        "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCYKtRnIfwiJkVic89Yt51Pw&type=video&order=date&maxResults=100&key=AIzaSyCYhHtLs42EUKEq5Jc6fR80_oF2penoDNg") as response:
     if response.getcode() == 200:
         source = response.read()
         data = json.loads(source)
@@ -101,7 +101,7 @@ with request.urlopen(
 
 
 with request.urlopen(
-        "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC9XUOGLM460u7Wb2d28DuaQ&type=video&order=date&maxResults=100&key=AIzaSyDnoxRyPtrO9We-Aa3ACMpluNag24-DLIc") as response:
+        "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC9XUOGLM460u7Wb2d28DuaQ&type=video&order=date&maxResults=100&key=AIzaSyCYhHtLs42EUKEq5Jc6fR80_oF2penoDNg") as response:
     if response.getcode() == 200:
         source = response.read()
         data = json.loads(source)
