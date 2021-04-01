3985ea70a5cac585f02239f26ae64ec49f3c6621
Officenne <officenne@gmail.com>
2021-04-01 11:14:31 +0000

    file updated

--- Android/EgyptApps/Badr_Family/Badr_Family.py
+++ Android/EgyptApps/Badr_Family/Badr_Family.py
@@ -25,6 +25,19 @@ with request.urlopen(
         print('An error occurred while attempting to retrieve data from the API.')
 
 with request.urlopen(
+        "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCzgVlXgVRC08y9TwaTY2owA&type=video&order=date&maxResults=100&key=AIzaSyDnnQb152LtaUR3dBCXNDoRbuV7cGdtb6I") as response:
+    if response.getcode() == 200:
+        source = response.read()
+        data = json.loads(source)
+        # print(data)
+        completeName = os.path.join(save_path, "Badr_Family2.json")
+        with open(completeName, 'w') as outfile:
+            json.dump(data, outfile, indent=4)
+    else:
+        print('An error occurred while attempting to retrieve data from the API.')
+
+
+with request.urlopen(
         "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCBA_nLOERvrGchrHHxYTqXQ&type=video&order=date&maxResults=100&key=AIzaSyDnnQb152LtaUR3dBCXNDoRbuV7cGdtb6I") as response:
     if response.getcode() == 200:
         source = response.read()
